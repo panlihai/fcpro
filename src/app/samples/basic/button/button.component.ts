795f41ba472d669a36bedfac95e41711d7c74459
pan.lh <hlhailan@163.com>
2018-02-01 16:19:31 +0800

    修改

--- src/app/samples/basic/button/button.component.ts
+++ src/app/samples/basic/button/button.component.ts
@@ -4,9 +4,37 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './button.component.html',
   styleUrls: ['./button.component.css']
 })
-export class ButtonComponent implements OnInit {
-  constructor() { }
-  ngOnInit() {
-  }
-
+export class ButtonComponent {
+  fcOptions = {
+    //皮肤默认为bootstrap风格
+    fcClass: 'ag-blue',
+    //是否启用查询
+    fcEnableSearch: false,
+    //是否启用排序
+    fcEnableSorting: true,
+    //是否启用过滤
+    fcEnableFilter: true,
+    //是否自动设置表头大小
+    fcEnableColResize: true,
+    //是否显示工具栏
+    fcShowToolPanel: true,
+    //是否分页
+    fcPagination: true,
+    //是否显示分组
+    fcRowGroupPanelShow: 'always',
+    //是否启用状态栏
+    fcEnableStatusBar: true,
+    //是否启用区域选中
+    fcEnableRangeSelection: true,
+    //选中方式
+    fcRowSelection: 'multiple',
+    //是否启用操作列
+    fcEnableAction: false,
+    //选中有checkbox
+    fcCheckboxSelection: true,
+    //是否启用编辑
+    fcEnableEdit: false,
+    //是否自动保存
+    fcAutoSave: false
+  };
 }
