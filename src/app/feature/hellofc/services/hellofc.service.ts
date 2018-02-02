7d56d0cdc6efeebee863a3598dd15eae3c1dbd1a
luohong <luohong@neusoft.com>
2018-02-02 12:20:36 +0800

    正在开发中

--- src/app/feature/hellofc/services/hellofc.service.ts
+++ src/app/feature/hellofc/services/hellofc.service.ts
@@ -1,6 +1,6 @@
 /* 	元数据 */
 import { Injectable } from '@angular/core';
-import { ParentService, ProvidersService } from 'fccore/index';
+import { ParentService, ProvidersService } from 'fccore';
 @Injectable()
 export class HellofcService extends ParentService {
   constructor(public providers: ProvidersService) {
--- src/app/samples/sys/sysauth/sysauth.component.ts
+++ src/app/samples/sys/sysauth/sysauth.component.ts
@@ -1,15 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { ComponentParent } from '../../componentparent';
+import { ComponentService } from '../../services/component.service';
 
 @Component({
   selector: 'app-sysauth',
   templateUrl: './sysauth.component.html',
   styleUrls: ['./sysauth.component.css']
 })
-export class SysauthComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class SysauthComponent extends ComponentParent {
+  constructor(public mainService: ComponentService) {
+    super('FCSYSAUTH', mainService);
   }
-
 }
