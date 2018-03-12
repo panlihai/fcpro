71c6494bd1bc4120ce8ffa62e2339e67ae335151
luohong <luohong@neusoft.com>
2018-03-12 13:45:58 +0800

    升级fccomponent@0.2.31升级listdata

--- src/app/samples/ad/adform/adform.component.ts
+++ src/app/samples/ad/adform/adform.component.ts
@@ -9,6 +9,18 @@ import { FcadformOption } from 'fccomponent';
 })
 export class AdformComponent extends ComponentParent {
   formConfig: FcadformOption = { fcTitle: "元数据编辑" };
+  radioValue: string = 'a';
+  anyValue: any = { "label": "A", "value": "a", "disabled": false };
+  anyOptions: any[] = [{ icon: '', label: 'A', value: 'a' }, { icon: '', label: 'B', value: 'b' }, { icon: '', label: 'C', value: 'c' }];
+  manyValue: any[] = [{ "label": "A", "value": "a", "disabled": false }];
+  checkValue: string = 'a';
+  checkOptions: any[] = [{ icon: '', label: 'A', value: 'a' }, { icon: '', label: 'B', value: 'b' }, { icon: '', label: 'C', value: 'c' }];
+  chosenValue: string = 'a';
+  chosenOptions: any[] = [{ icon: '', label: 'A', value: 'a' }, { icon: '', label: 'B', value: 'b' }, { icon: '', label: 'C', value: 'c' }];
+  comboValue: string = 'a';
+  comboOptions: any[] = [{ icon: '', label: 'A', value: 'a' }, { icon: '', label: 'B', value: 'b' }, { icon: '', label: 'C', value: 'c' }];
+  now = new Date();
+  inputNumber: number = 3;
   constructor(public mainService: ComponentService) {
     super('FCADFORM', mainService);
   }
