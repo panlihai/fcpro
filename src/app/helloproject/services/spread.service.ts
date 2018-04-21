fd42d19b92a5f3c6bea3315fb64066a0a45d7542
panlihai <hlhailan@163.com>
2018-04-21 14:44:09 +0800

    增加

--- src/app/helloproject/services/spread.service.ts
+++
@@ -1,9 +0,0 @@
-/* 	spreadjs */
-import { Injectable } from '@angular/core';
-import { ParentService, ProvidersService } from 'fccore';
-@Injectable()
-export class SpreadService extends ParentService {
-  constructor(public providers: ProvidersService) {
-    super(providers, "SYSCOMPONENT");
-  }
-}
