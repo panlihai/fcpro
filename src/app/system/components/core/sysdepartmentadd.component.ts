10b1a74a743f2dd232e89271c756fcdec779af32
WindIvI <13972848@qq.com>
2018-07-09 15:38:00 +0800

    update

--- src/app/system/components/core/sysdepartmentadd.component.ts
+++ src/app/system/components/core/sysdepartmentadd.component.ts
@@ -1,10 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
-import { ParentEditComponent } from 'fccomponent';
-import { SysdepartmentService } from '../../services/sysdepartment.service';
+import { ParentEditComponent, FclistdataComponent, FcdateComponent } from 'fccomponent';
+import { SysdepartmentService, Args_Dept, EmployList_State } from '../../services/sysdepartment.service';
 import { FCEVENT } from 'fccomponent/fc';
 import { environment } from '../../../../environments/environment';
 import { ProvidersService } from 'fccore';
+import { GridApi, ColumnApi } from 'ag-grid';
+import * as differenceInCalendarDays from 'date-fns/difference_in_calendar_days';
 @Component({
   selector: 'sysdepartmentadd',
   templateUrl: './sysdepartmentadd.component.html',
@@ -13,8 +15,17 @@ import { ProvidersService } from 'fccore';
   `]
 })
 export class SysdepartmentaddComponent extends ParentEditComponent {
+  employeeList_state: any;
   //单位隶属关系
   sysdepartmentrelationObj: any = {};
+  employeeDept_listcond: any;
+  employee_listcond: any;
+  @ViewChild('employeeList') employeeList: FclistdataComponent;
+  @ViewChild('employeeList_dept') employeeList_dept: FclistdataComponent;
+  @ViewChild('deptRelationList') deptRelationList: FclistdataComponent;
+  @ViewChild('contentTplEmployee') contentTplEmployee: FclistdataComponent;
+  @ViewChild('footerEmployee') footerEmployee: FclistdataComponent;
+  @ViewChild('beginDate') beginDate: FcdateComponent;
   constructor(public mainService: SysdepartmentService,
     public router: Router,
     public activeRoute: ActivatedRoute,
@@ -25,7 +36,9 @@ export class SysdepartmentaddComponent extends ParentEditComponent {
   init() {
     //初始化单位隶属关系字段
     this.sysdepartmentrelationObj = this.mainService.getSysdepartmentrelationField();
+    this.initComponent();
   }
+
   addNew(mainObj: any): boolean {
     return true;
   }
@@ -59,6 +72,48 @@ export class SysdepartmentaddComponent extends ParentEditComponent {
           PID: environment.pid, MENUTYPE: 'INURL', MENUNAME: '单位调整', MENUICON: 'fc-icon-bgefficiency'
         });
         break;
+    }
+  }
+  ngAfterViewInit() {
+    //Called after ngAfterContentInit when the component's view has been initialized. Applies to components only.
+    //Add 'implements AfterViewInit' to the class.
+    setTimeout(() => {
+      let columnApi: ColumnApi = this.employeeList_dept._gridColumnApi
+      columnApi.setColumnVisible('ROWNUM', false);
+    });
+  }
+  initComponent() {
+    this.employeeDept_listcond = { where: `SDEPT_CODE = '${this.mainObj.SDEPT_CODE}` };
+    this.employee_listcond = { where: `SDEPT_CODE is null or SDEPT_CODE = ''` };
+    this.employeeList_state = EmployList_State.noValue;
+  }
+  employee_add(args: Args_Dept) {
+    if (this.mainObj.SDEPT_CODE) {
+      args = { titleTpl: '新增部门职员', contentTpl: this.contentTplEmployee, footerTpl: this.footerEmployee };
+      this.employee_listcond = this.mainService.beforeEmployee_add(this.employeeList_dept);
+      this.mainService.open(args);
+    } else {
+      this.messageService.warm("部门编码不存在！")
+    }
+  }
+  employee_del() {
+    this.mainService.employee_del(this.employeeList_dept);
+    this.checkEmployeeList();
+  }
+  employeeDialog_cancel() {
+    this.mainService.dialogCancel();
+  }
+  employeeDialog_ok() {
+    this.mainService.dialogOk(this.employeeList, this.employeeList_dept, { key: 'SDEPT_CODE', value: this.mainObj.SDEPT_CODE });
+    this.checkEmployeeList();
+  }
+  checkEmployeeList() {
+    this.employeeList_state = this.mainService.checkEmployeeList(this.employeeList_dept)
+  }
+  dateChanged(ev) {
+    this.mainObj.SBEGIN_DATE = '';
+    this.beginDate._disabledDate = (current) => {
+      return differenceInCalendarDays(ev, current) > 0
     }
   }
 }
--- src/app/system/services/sysdepartment.service.ts
+++ src/app/system/services/sysdepartment.service.ts
@@ -5,12 +5,104 @@ import { SysdepartmentrelationService } from './sysdepartmentrelation.service';
 import { SystbvdeptcurorgService } from './systbvdeptcurorg.service';
 import { Observable } from 'rxjs';
 import { TreeOptions } from 'fccomponent/fcbasic';
+import { NzModalService, NzModalSubject } from 'ng-zorro-antd';
+import { FclistdataComponent } from 'fccomponent';
+import { GridApi, ColumnApi, Grid } from 'ag-grid';
+import { RowDataTransaction } from 'ag-grid/dist/lib/rowModels/inMemory/inMemoryRowModel';
 @Injectable()
 export class SysdepartmentService extends ParentService {
+  currentModal: NzModalSubject;
   constructor(public providers: ProvidersService,
     public sysdeptrelationService: SysdepartmentrelationService,
-    public systbvdeptcurorgService: SystbvdeptcurorgService) {
+    public systbvdeptcurorgService: SystbvdeptcurorgService,
+    public nzModal: NzModalService, ) {
     super(providers, "SYSDEPARTMENT");
+  }
+  beforeEmployee_add(employeeList_dept: FclistdataComponent) {
+    let gridApi: GridApi = employeeList_dept._gridApi;
+    let ids: any = [];
+    let rowData = gridApi.getRenderedNodes()
+    if (rowData) {
+      rowData.forEach(el => {
+        ids.push(el.data.ID);
+      })
+    }
+    if (ids && ids.length !== 0) {
+      let s = this.arrayToSqlString(ids)
+      return { WHERE: `ID NOT IN (${s})` }
+    } else return {};
+  }
+  /** YM
+ * 数组转sql批查询条件
+ * @param array
+ */
+  arrayToSqlString(array: Array<any>) {
+    let str: string = "";
+    for (let i = 0; i < array.length; i++) {
+      str += `'${array[i]}'`;
+      if (i !== array.length - 1) {
+        str += ",";
+      }
+    }
+    return str.toString();
+  }
+  open(args: Args_Dept) {
+    if (args) {
+      this.currentModal = this.nzModal.open({
+        title: args.titleTpl ? args.titleTpl : null,
+        content: args.contentTpl ? args.contentTpl : null,
+        footer: args.footerTpl ? args.footerTpl : null,
+        style: { width: "60%" },
+        onOk: () => { },
+        onCancel: () => { }
+      })
+      return true;
+    } else {
+      return false;
+    }
+  }
+  checkEmployeeList(listdata_dept: FclistdataComponent) {
+    let gridApi: GridApi = listdata_dept._gridApi;
+    let cond: string = EmployList_State.noValue;
+    gridApi.forEachNode(el => {
+      cond = EmployList_State.existValue;
+    })
+    return cond;
+  }
+
+  dialogCancel() {
+    this.currentModal.destroy();
+    this.currentModal = null;
+  }
+  dialogOk(employeeList: FclistdataComponent, employeeList_dept: FclistdataComponent, changeObj: { key: any, value: any }) {
+    let gridApi: GridApi = employeeList._gridApi;
+    let gridApi_depr: GridApi = employeeList_dept._gridApi;
+    let selected = gridApi.getSelectedRows();
+    let toChange: RowDataTransaction = {};
+    selected.forEach(el => {
+      for (let index in el) {
+        if (index === changeObj.key) {
+          el[index] = changeObj.value
+        }
+      }
+    })
+    toChange.add = selected;
+    gridApi_depr.updateRowData(toChange)
+    this.currentModal.destroy();
+    this.currentModal = null;
+  }
+  employee_del(employeeList_dept) {
+    let gridApi: GridApi = employeeList_dept._gridApi;
+    let selected = gridApi.getSelectedRows();
+    let toChange: RowDataTransaction = {};
+    toChange.remove = selected;
+    gridApi.updateRowData(toChange)
+  }
+  /** YM
+ *  初始化DefaultObj
+ */
+  getDefaultObj() {
+    return this.providers.appService.initObjDefaultValue(this.app);
   }
   /**
    * 根据id获取主对象的编辑数据
@@ -34,6 +126,7 @@ export class SysdepartmentService extends ParentService {
       return item;
     });
   }
+
   departmentTreeOptions: TreeOptions = {
     //元数据id
     fcAppid: "SYSTBVDEPTCURORG",//元数据id
@@ -95,6 +188,41 @@ export class SysdepartmentService extends ParentService {
       this.sysdeptrelationService.save(relationObj)
     );
   }
+  employeeListOptions = {
+    //皮肤默认为bootstrap风格
+    fcClass: 'ag-blue',
+    //每页显示条数
+    fcPaginationPageSize: 20,
+    //是否启用查询
+    fcEnableSearch: false,
+    //是否启用排序
+    fcEnableSorting: true,
+    //是否启用过滤
+    fcEnableFilter: true,
+    //是否自动设置表头大小
+    fcEnableColResize: true,
+    //是否显示工具栏
+    fcShowToolPanel: false,
+    //是否分页
+    fcPagination: true,
+    //是否显示分组
+    fcRowGroupPanelShow: 'none',//'always',
+    //是否启用状态栏
+    fcEnableStatusBar: true,
+    //是否启用区域选中
+    fcEnableRangeSelection: false,
+    //选中方式
+    fcRowSelection: 'multiple',
+    //是否启用操作列
+    fcEnableAction: false,
+    //选中有checkbox
+    fcCheckboxSelection: true,
+    //是否启用编辑
+    fcEnableEdit: false,
+    //是否自动保存
+    fcAutoSave: false,
+    fcAutoSize: false
+  };
   //列表
   fclistdataOption = {
     //皮肤默认为bootstrap风格
@@ -132,6 +260,40 @@ export class SysdepartmentService extends ParentService {
     fcAutoSize: false
   };
   //
+  sysemployeeOption = {
+    //皮肤默认为bootstrap风格
+    fcClass: 'ag-blue',
+    //每页显示条数
+    fcPaginationPageSize: 20,
+    //是否启用查询
+    fcEnableSearch: false,
+    //是否启用排序
+    fcEnableSorting: true,
+    //是否启用过滤
+    fcEnableFilter: true,
+    //是否自动设置表头大小
+    fcEnableColResize: true,
+    //是否显示工具栏
+    fcShowToolPanel: false,
+    //是否分页
+    fcPagination: false,
+    //是否显示分组
+    fcRowGroupPanelShow: 'none',//'always',
+    //是否启用状态栏
+    fcEnableStatusBar: true,
+    //是否启用区域选中
+    fcEnableRangeSelection: false,
+    //选中方式
+    fcRowSelection: 'multiple',
+    //是否启用操作列
+    fcEnableAction: false,
+    //选中有checkbox
+    fcCheckboxSelection: true,
+    //是否启用编辑
+    fcEnableEdit: false,
+    //是否自动保存
+    fcAutoSave: false
+  };
   sysdepartmentrelationOption = {
     //皮肤默认为bootstrap风格
     fcClass: 'ag-blue',
@@ -148,7 +310,7 @@ export class SysdepartmentService extends ParentService {
     //是否显示工具栏
     fcShowToolPanel: false,
     //是否分页
-    fcPagination: true,
+    fcPagination: false,
     //是否显示分组
     fcRowGroupPanelShow: 'none',//'always',
     //是否启用状态栏
@@ -167,7 +329,10 @@ export class SysdepartmentService extends ParentService {
     fcAutoSave: false
   };
 }
-
+export const EmployList_State = {
+  existValue: 'existValue',
+  noValue: 'noValue'
+}
 export interface Sysdepartment {
   ID: string;	//
   PID: string;	//
@@ -192,4 +357,9 @@ export interface Sysdepartment {
   GROUP_CODE: string;	//
   GROUP_NAME: string;	//
   ILEVEL: number;	//
+}
+export interface Args_Dept {
+  titleTpl: any,
+  contentTpl?: any,
+  footerTpl?: any,
 }
