af19b704452e8b688ac0f06f2077aeed4510dac9
sundanhua <sundanhua@neusoft.com>
2018-07-19 09:35:49 +0800

    update

--- src/app/system/components/core/sysappadd.component.ts
+++ src/app/system/components/core/sysappadd.component.ts
@@ -7,24 +7,28 @@ import { Sysappbuttons } from 'fccore/services/sysappbuttons.service';
 @Component({
   selector: 'sysappadd',
   template: `
-  <fc-layoutpanel>
+  <fc-layoutpanel class="sysappadd">
       <fc-title fcLabel="数据源" fccontent></fc-title>
       <fc-layoutcol fcSpans="1,1" fccontent>  
         <fc-layoutcol fcSpans="1,1" fccontent1>  
-               
+          <fc-combo [fcLabel]="'产品'" [fcOption]="productOption" [(ngModel)]="productValue" fccontent1>
+          </fc-combo>
+          <fc-combo [fcLabel]="'数据源'" [fcOption]="datasourceOption" [(ngModel)]="datasourceValue" fccontent2>
+          </fc-combo>    
         </fc-layoutcol>
       </fc-layoutcol>  
-      <fc-layoutcol fcSpans="1,1" fccontent>
+      <fc-layoutcol fcSpans="1,1" fccontent class="systransfer">
         <nz-transfer fccontent1
             [nzDataSource]="list"
-            [nzListStyle]="{'width.px': 200, 'height.px': 400}"
+            [nzTitles]="['所有表及视图', '选中的表或视图']"
+            [nzListStyle]="{'width.px': 240, 'height.px': 400}"
             (nzSelectChange)="select($event)"
             (nzChange)="change($event)">
             <ng-template #render let-item>
-              <i class="anticon anticon-{{item.icon}}"></i> {{item.title}}
+              {{item.title}}
             </ng-template>
         </nz-transfer>
-        <fc-button fccontent2 fcLabel="生成元数据" [fcType]="'default'" class='sys-block'></fc-button>
+        <fc-button fccontent2 fcLabel="一键生成" [fcType]="'default'" class='sys-block'></fc-button>
         <fc-button fccontent2 fcLabel="查看所有" [fcType]="'primary'"></fc-button>
       </fc-layoutcol>
   </fc-layoutpanel>
@@ -33,12 +37,25 @@ import { Sysappbuttons } from 'fccore/services/sysappbuttons.service';
   :host ::ng-deep .sys-block .ant-btn{
     display: block;
     margin-bottom:20px;
+  } 
+  :host ::ng-deep .sysappadd .fc-layoutpanel {
+    height: 100%;
+  }
+  :host ::ng-deep .systransfer .fc-content1 {
+    padding-left: 45px;
+  }
+  :host ::ng-deep .systransfer .fc-layoutcol{
+    padding-top: 25px;
   }
   `]
 })
 export class SysappaddComponent extends ParentEditComponent {
+  //产品下拉选项
+  productOption: any[] = [];
+  //数据源下拉选项
+  datasourceOption: any[] = [];
+  //穿梭图list
   list: any[] = [];
-  tlbBtns:Sysappbuttons[];
   constructor(public mainService: SysappService,
     public router: Router,
     public activeRoute: ActivatedRoute, 
@@ -47,12 +64,26 @@ export class SysappaddComponent extends ParentEditComponent {
     super(mainService, router, activeRoute);
   }
   init(): void {
+    //初始化获取所有软件产品的产品名称
+    this.mainService.getProduct({}).subscribe(res=>{
+      res.P_LISTVALUE.forEach(element => {
+        //将获得的产品名称添加到下拉框中
+        this.productOption.push({ icon: '', label: element.PNAME, value: element.PNAME });
+      });
+    })
+    //初始化获取所有数据源
+    this.mainService.getdataSource({}).subscribe(result=>{
+      result.P_LISTVALUE.forEach(element => {
+        //将获得的数据源名称添加到下拉框中
+        this.datasourceOption.push({ icon: '', label: element.DSNAME, value: element.PID });
+      });
+    })
   }
   ngOnInit(): void {
     //获取数据源
     this.getData();
   }
-   /**
+  /**
   * 获取数据源
   */
  getData(): void {
@@ -68,17 +99,14 @@ export class SysappaddComponent extends ParentEditComponent {
   }
   this.list = ret;
 }
-
-select(ret: any) {
-  console.log('nzSelectChange', ret);
-}
-
-change(ret: any) {
-  console.log('nzChange', ret);
-}
   addNew(mainObj: any): boolean {
     return true;
   }
   event(eventName: string, param: any): void {
+  }
+  select(ret: any) {
+  }
+
+  change(ret: any) {
   }
 }
--- src/app/system/services/sysapp.service.ts
+++ src/app/system/services/sysapp.service.ts
@@ -22,4 +22,44 @@ export class SysappService extends ParentService {
       }
     });
   }
+  /**
+   * 获取所有软件产品的产品名称
+   * @param {page:1,size:20,...}
+   * @description 查詢
+   */
+  getProduct(param: any): Observable<any> {
+    if (!param.hasOwnProperty('P_COUNT')) {
+      param.P_COUNT = 1;
+    }
+    if (!param.hasOwnProperty('MAINAPP')) {
+      param.MAINAPP = '';
+    }
+    if (!param.hasOwnProperty('MAINAPPID')) {
+      param.MAINAPPID = '';
+    }
+    if (!param.hasOwnProperty('P_LISTFILTER')) {
+      param.P_LISTFILTER = '';
+    }
+    return this.providers.daoService.getFromSystem("ajax/SYSTEM/SYSTEMPRODUCT/SYSPRODUCT/listView", param);
+  }
+  /**
+   * 获取所有数据源
+   * @param {page:1,size:20,...}
+   * @description 查詢
+   */
+  getdataSource(param: any): Observable<any> {
+    if (!param.hasOwnProperty('P_COUNT')) {
+      param.P_COUNT = 1;
+    }
+    if (!param.hasOwnProperty('MAINAPP')) {
+      param.MAINAPP = '';
+    }
+    if (!param.hasOwnProperty('MAINAPPID')) {
+      param.MAINAPPID = '';
+    }
+    if (!param.hasOwnProperty('P_LISTFILTER')) {
+      param.P_LISTFILTER = '';
+    }
+    return this.providers.daoService.getFromSystem("ajax/SYSTEM/SYSDATASOURCE/SYSDATASOURCE/listView", param);
+  }
 }
