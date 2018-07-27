c454b4bb378b210603d1354cda23adbdd7e5aad3
Ssunjiaying <348898934@qq.com>
2018-07-27 14:42:07 +0800

    更新全面预算代码

--- src/app/system/services/sysdatasource.service.ts
+++ src/app/system/services/sysdatasource.service.ts
@@ -2,9 +2,13 @@
 import { Injectable } from '@angular/core';
 import { ParentService, ProvidersService } from 'fccore';
 import { Observable } from 'rxjs/Observable';
+import { NzModalService } from 'ng-zorro-antd';
+import { SysproductService } from './sysproduct.service';
 @Injectable()
 export class SysdatasourceService extends ParentService {
-  constructor(public providers: ProvidersService) {
+  constructor(public providers: ProvidersService,
+    public sysproductService: SysproductService,
+    public modalService: NzModalService) {
     super(providers, "SYSDATASOURCE");
   }
     /**
@@ -138,5 +142,27 @@ export class SysdatasourceService extends ParentService {
       param.P_LISTFILTER = '';
     }
     return this.providers.daoService.getFromSystem("ajax/SYSTEM/SYSDATASOURCE/SYSDATASOURCE/listView", param);
+  }
+   /**
+    *  按钮跳转路由方法封装 查看数据源  查看服务   返回列表 方法
+    * @param event  
+    */ 
+   producticonmodal(content): Observable<any> {
+    return this.modalService.open({
+      title: '字体图标',
+      content: content,
+      width: "60%",
+      onOk() {
+      },
+      onCancel() { },
+      footer: false,
+      componentParams: {
+        options: {
+        }
+      }
+    })
+  }
+  dataall(){
+    return this.sysproductService.findWithQuery({})
   }
 }
